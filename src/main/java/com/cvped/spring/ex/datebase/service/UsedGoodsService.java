308dbabacfcf5bc36e17ee78be3020ad93499750
cvped <cvped@gmail.com>
2024-11-01 18:58:23 +0900

    [신재경] 데이터베이스 연동 예제

---
+++ src/main/java/com/cvped/spring/ex/datebase/service/UsedGoodsService.java
@@ -0,0 +1,28 @@
+package com.cvped.spring.ex.datebase.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cvped.spring.ex.datebase.domain.UsedGoods;
+import com.cvped.spring.ex.datebase.repository.UsedGoodsRepository;
+
+// 로직(business)을 담당
+// 데이터를 수정 가공
+// 그외 복잡한 기능 수행
+@Service
+public class UsedGoodsService {
+
+	@Autowired
+	private UsedGoodsRepository usedGdoodsRepository;
+	// 중고물품 게시글 모두 얻어 오기
+	public getUsedGoodsList() {
+		
+		// usedgoods 테이블 조회 결과 얻어 오기
+		List<UsedGoods> usedGoodsList = usedGoodsRepository.selectUsedGoodsList();
+		
+		return usedGoodsList;
+		
+	}
+}
